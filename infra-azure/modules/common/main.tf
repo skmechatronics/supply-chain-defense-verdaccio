4027bea76a5aba1c6d0e4075ad32ac8a637dab54
Srikanth K <sk.mechatronics@outlook.com>
2026-05-17 15:02:19 +1000

    feat: add common terraform module for resource group, storage, and ACR

---
+++ infra-azure/modules/common/main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg-${var.location_abbr}"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "verdaccio" {
+  name                            = "${var.prefix}stacc${var.location_abbr}"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  min_tls_version                 = "TLS1_2"
+  https_traffic_only_enabled      = true
+  allow_nested_items_to_be_public = false
+}
+
+resource "azurerm_storage_share" "verdaccio" {
+  name               = "verdaccio-storage"
+  storage_account_id = azurerm_storage_account.verdaccio.id
+  quota              = var.storage_share_quota_gb
+}
+
+resource "azurerm_container_registry" "main" {
+  name                = "${var.prefix}acr${var.location_abbr}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
---
+++ infra-azure/modules/common/outputs.tf
@@ -0,0 +1,34 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.main.location
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.verdaccio.name
+}
+
+output "storage_account_key" {
+  value     = azurerm_storage_account.verdaccio.primary_access_key
+  sensitive = true
+}
+
+output "storage_share_name" {
+  value = azurerm_storage_share.verdaccio.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.main.login_server
+}
+
+output "acr_admin_username" {
+  value     = azurerm_container_registry.main.admin_username
+  sensitive = true
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.main.admin_password
+  sensitive = true
+}
